cdfe552f8af8b9666325316f9056fc13b7d04511
김태훈 <xognsrla0102@naver.com>
2022-03-21 02:07:47 +0900

    프로젝트 세팅

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<GameManager>();
+            }
+            return instance;
+        }
+    }
+    
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
---
+++ Assets/Scripts/Janggi.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Janggi_Type
+{
+    JA,
+    HOO,
+    JANG,
+    SANG,
+    WANG,
+    JANGGI_TYPE_CNT
+}
+
+public abstract class Janggi : MonoBehaviour
+{
+    [SerializeField] private Janggi_Type janggiType;
+
+    private void Start()
+    {
+        
+    }
+
+    
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        switch (janggiType)
+        {
+            case Janggi_Type.JA:
+                break;
+                case Jan
+        }
+    }
+}
